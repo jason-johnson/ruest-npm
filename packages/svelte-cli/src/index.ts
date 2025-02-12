5292eaab8e413f369d33dcdb5a12b6c90326e3a9
Jason <jason.johnson.081@gmail.com>
2025-02-12 22:27:46 +0100

    refactor: remove old Svelte package and introduce CLI for Ruest integration

---
+++ packages/svelte-cli/src/index.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv))
+  .usage("usage: $0 <Ruest server address>")
+  .demandCommand(1, "Ruest compatible server address is required")
+  .help(true)
+  .parseSync();
+
+const serverAddress = argv._[0] as string;
+const serverUrl = new URL("/.schema.json", serverAddress);
+
+console.log(serverUrl.toString());
+
+fetch(serverUrl).then(async (response) => {
+  if (!response.ok) {
+    console.error(`Failed to fetch schema from ${serverUrl}: ${response.statusText}`);
+    process.exit(1);
+  }
+
+  const schema = await response.json();
+  console.log(schema);
+});
